15b401d821eb2f48522e4e6f8dbcd264afa7b0e2
Meg Cruzen <megcruzen@gmail.com>
2019-02-03 15:37:34 -0600

    Styling CostumeCard

--- src/components/costume/CostumeCard.js
+++ src/components/costume/CostumeCard.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
-// import { Link } from 'react-router-dom'
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 
 export default class CostumeCard extends Component {
     render() {
         return (
-            <div key={this.props.costume.id}>
-                <div>
-                {this.props.costume.name}
-                </div>
-                {/* <Link className="nav-link" to={`students/${student.id}/edit`}>Edit</Link> */}
-            </div>
+                <Card key={this.props.costume.id} className="mr-2 mb-3">
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                    <CardBody>
+                        <CardSubtitle className="text-uppercase series">{this.props.costume.series}</CardSubtitle>
+                        <CardTitle><h3>{this.props.costume.name}</h3></CardTitle>
+                        <CardText className="outfit mt-1"><i class="fas fa-user-circle"></i> {this.props.costume.outfit}</CardText>
+                    </CardBody>
+                </Card>
         )
     }
 }
--- src/components/costume/CostumeList.js
+++ src/components/costume/CostumeList.js
@@ -4,9 +4,9 @@ import CostumeCard from "./CostumeCard"
 export default class CostumeList extends Component {
     render() {
         return (
-            <section>
+            <section className="costume_list">
                 <h1>My Costumes</h1>
-                <div className="costume_list_global">
+                <div className="d-flex justify-content-between flex-wrap">
                 {
                     this.props.costumes.map(costume =>
                         <CostumeCard key={costume.id} costume={costume} {...this.props} />

22a1d7c5eb79a78231b37318621069d1e57df882
Meg Cruzen <megcruzen@gmail.com>
2019-02-17 06:48:22 -0600

    Used react-collapsible to create collapsable Member List and Character List

--- src/components/group/CharacterList.js
+++ src/components/group/CharacterList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Table, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import Collapsible from 'react-collapsible';
 import CharacterCard from './CharacterCard'
 
 export default class CharacterList extends Component {
@@ -10,10 +11,8 @@ export default class CharacterList extends Component {
         groupId: ""
     }
 
-    // Update state whenever an input field is edited
     handleFieldChange = evt => {
         const stateToChange = {}
-        // console.log(evt.target.id, evt.target.value);
         stateToChange[evt.target.id] = evt.target.value
         this.setState(stateToChange)
     }
@@ -35,25 +34,28 @@ export default class CharacterList extends Component {
 
         return (
                 <div className="character_list mb-2">
-                    <h5>Character List</h5>
-                    <Table>
-                        <tbody>
-                        {
-                            this.props.characters.filter(character => character.groupId === this.props.myGroup.id)
-                            .map(character =>
-                                <CharacterCard character={character} deleteCharacter={this.props.deleteCharacter} updateCharacter={this.props.updateCharacter} groupId={this.props.myGroup.id} />
-                            )
-                        }
-                        </tbody>
-                    </Table>
-                    <Form onSubmit={this.addCharacter} className="d-flex">
-                        <FormGroup className="w-100 mr-2">
-                            <Label for="character" hidden>Character Name</Label>
-                            <Input type="text" required name="character" id="character"
-                            onChange={this.handleFieldChange} placeholder="Add character" />
-                        </FormGroup>
-                        <div><Button color="primary">Add</Button></div>
-                    </Form>
+
+                    <Collapsible trigger="Character List">
+                        <Table>
+                            <tbody>
+                            {
+                                this.props.characters.filter(character => character.groupId === this.props.myGroup.id)
+                                .map(character =>
+                                    <CharacterCard character={character} deleteCharacter={this.props.deleteCharacter} updateCharacter={this.props.updateCharacter} groupId={this.props.myGroup.id} />
+                                )
+                            }
+                            </tbody>
+                        </Table>
+                        <Form onSubmit={this.addCharacter} className="d-flex">
+                            <FormGroup className="w-100 mr-2">
+                                <Label for="character" hidden>Character Name</Label>
+                                <Input type="text" required name="character" id="character"
+                                onChange={this.handleFieldChange} placeholder="Add character" />
+                            </FormGroup>
+                            <div><Button color="primary">Add</Button></div>
+                        </Form>
+                    </Collapsible>
+
                 </div>
 
         )
--- src/components/group/MemberList.js
+++ src/components/group/MemberList.js
@@ -1,34 +1,29 @@
 import React, { Component } from 'react';
 import { Button, Collapse } from 'reactstrap';
+import Collapsible from 'react-collapsible';
 
 export default class MemberList extends Component {
 
-    constructor(props) {
-        super(props);
-        this.toggle = this.toggle.bind(this);
-        this.state = { collapse: false };
-      }
+    state = { collapse: false }
 
-      toggle() {
+    toggle = () => {
         this.setState({ collapse: !this.state.collapse });
-      }
+    }
 
     render() {
         return (
                 <div sm="6" className="member_list mb-2">
-                    <div>
-                        <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>View Member List</Button>
-                        <Collapse isOpen={this.state.collapse}>
-                            <ul>
+
+                    <Collapsible trigger="Member List">
+                        <ul>
                             {
                                 this.props.groupMembers.filter(member => member.groupId === this.props.myGroup.id)
                                 .map(member =>
                                     <li>{member.user.username}</li>
                                 )
                             }
-                            </ul>
-                        </Collapse>
-                    </div>
+                        </ul>
+                    </Collapsible>
 
                 </div>
 
